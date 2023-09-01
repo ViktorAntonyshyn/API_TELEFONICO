2da15398ac927e4d4b9f3467e768387e1ad9d3ca
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-01 10:27:39 +0200

    nothing importantes

--- src/main/java/com/example/PROYECTO_FULLSTACK/apiControllers/CitaController.java
+++ src/main/java/com/example/PROYECTO_FULLSTACK/apiControllers/CitaController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(path = "api/v1/citas")
